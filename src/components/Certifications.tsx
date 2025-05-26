01949e72b9d4253139818d22ad9413de432d2dbf
Supragna-2705 <140143518+Supragna-2705@users.noreply.github.com>
2025-05-26 20:27:08 +0530

    Update Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -30,7 +30,7 @@ export const Certifications: React.FC = () => {
   
   const achievements = [
     "Secured 10.0 in 10th grade",
-    "Achieved 982/1000 in Intermediate"
+    "Achieved 982/1000 in Intermediate",
     "Achieved 4th place in a national level Hackathon-HACKFINITI"
   ];
 
